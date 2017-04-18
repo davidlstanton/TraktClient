85716d586971422b71edadf70efc903297a57f33
David@Nattr <david@nattr.com>
2017-04-18 15:18:59 +0100

    added a MediaObjectViewModel making sure we decouple the ui from network

--- TraktClient/MediaObjectViewModel.swift
+++ TraktClient/MediaObjectViewModel.swift
@@ -7,12 +7,70 @@
 //
 
 import UIKit
+import IGListKit
 
-class MediaObjectView {
+class MediaObjectViewModel: IGListDiffable, CustomStringConvertible {
 
-    let id: Int
+    let traktId: Int
+    let title: String
+    let year: Int
+    let overview: String
+    let rating: Double
+    let genres: [String]
+    let posterUrlString: String
+    let thumbUrlString: String
     
-    init(id: Int) {
-        <#statements#>
+    init?(standardMediaObject: StandardMediaObject, posterImage: MediaObjectImage, thumbImage: MediaObjectImage) {
+        traktId = standardMediaObject.ids.trakt
+        title = standardMediaObject.title
+        year = standardMediaObject.year
+        guard
+            let overview = standardMediaObject.overview,
+            let rating = standardMediaObject.rating,
+            let genres = standardMediaObject.genres
+        else { return nil }
+        self.overview = overview
+        self.rating = rating
+        self.genres = genres
+        posterUrlString = posterImage.url
+        thumbUrlString = thumbImage.url
+    }
+    
+    init(traktId: Int,
+         title: String,
+         year: Int,
+         overview: String,
+         rating: Double,
+         genres: [String],
+         posterUrlString: String,
+         thumbUrlString: String) {
+        self.traktId = traktId
+        self.title = title
+        self.year = year
+        self.overview = overview
+        self.rating = rating
+        self.genres = genres
+        self.posterUrlString = posterUrlString
+        self.thumbUrlString = thumbUrlString
+    }
+    
+    func diffIdentifier() -> NSObjectProtocol {
+        return NSNumber(integerLiteral: traktId)
+    }
+    
+    func isEqual(toDiffableObject object: IGListDiffable?) -> Bool {
+        if let object = object as? MediaObjectViewModel {
+            if Double(object.traktId) + object.rating == Double(traktId) + rating {
+                if (object.title + object.overview + object.posterUrlString + object.thumbUrlString) ==
+                    (title + overview + posterUrlString + thumbUrlString) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+    
+    var description: String {
+        return "traktId: \(traktId)\ntitle: \(title)\nyear: \(year)\noverview: \(overview)\nrating: \(rating)\ngenres: \(genres.flatMap({$0}).joined(separator: ", "))\nposterUrlString: \(posterUrlString)\nthumbUrlString: \(thumbUrlString)"
     }
 }
