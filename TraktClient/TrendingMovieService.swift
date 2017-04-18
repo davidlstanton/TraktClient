d726b2a083fbb008674fa1351128b5dcbf0f6876
David@Nattr <david@nattr.com>
2017-04-18 17:13:34 +0100

    Added a service to provide view models

--- TraktClient/TrendingMovieService.swift
+++ TraktClient/TrendingMovieService.swift
@@ -7,7 +7,41 @@
 //
 
 import UIKit
+import Alamofire
 
-class TrendingMovieService: NSObject {
+class TrendingMovieService {
 
+    class func trendingMovies(page: Pagination, callback: @escaping ([MediaObjectViewModel])->()) {
+        var mediaObjectViewModels: [MediaObjectViewModel] = []
+        
+        Alamofire.request(TraktRouter.moviesTrending(pagination: page, extendedInfo: .full)).responseCollection {
+            (response: DataResponse<[StandardMediaObject]>) in
+            if let standardMediaObjects = response.result.value {
+                let group = DispatchGroup()
+                standardMediaObjects.forEach { (smo) in
+                    group.enter()
+                    Alamofire.request(FanArtRouter.movie(ids: smo.ids)).responseObject {
+                        (response: DataResponse<MovieMediaObjectImages>) in
+                        if let mediaObjects = response.result.value {
+                            guard
+                                let posterImage = mediaObjects.moviePoster?.first,
+                                let thumbImage = mediaObjects.movieThumb?.first
+                            else { return }
+                            if let mediaObject = MediaObjectViewModel(
+                                standardMediaObject: smo,
+                                posterImage: posterImage,
+                                thumbImage: thumbImage){
+                                mediaObjectViewModels.append(mediaObject)
+                            }
+                        }
+                        group.leave()
+                    }
+                }
+                group.notify(queue: DispatchQueue.main) {
+                    callback(mediaObjectViewModels)
+                }
+            }
+        }
+    }
+    
 }
