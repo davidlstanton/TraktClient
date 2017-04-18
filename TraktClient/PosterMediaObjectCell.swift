297d1e2d7d0de2b1b5bbd800fc6b6998f4aff5bc
David@Nattr <david@nattr.com>
2017-04-18 17:11:25 +0100

    added a cell for poster cells

--- TraktClient/PosterMediaObjectCell.swift
+++ TraktClient/PosterMediaObjectCell.swift
@@ -10,4 +10,10 @@ import UIKit
 
 class PosterMediaObjectCell: UICollectionViewCell {
     
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    
+    func setCell(with mediaObject: MediaObjectViewModel) {
+        imageView.af_setImage(withURLRequest: FanArtAssetRouter.full(urlString: mediaObject.posterUrlString))
+    }
 }
