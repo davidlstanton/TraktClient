739e80391945e94d7463bf4c7afc1dd0c2003e93
David@Nattr <david@nattr.com>
2017-04-18 17:13:01 +0100

    Added a section view for showing media object thumb images

--- TraktClient/ThumbMediaObjectSectionView.swift
+++ TraktClient/ThumbMediaObjectSectionView.swift
@@ -7,7 +7,36 @@
 //
 
 import UIKit
+import IGListKit
 
-class ThumbMediaObjectSectionView: NSObject {
-
+class ThumbMediaObjectSectionView: IGListSectionController, IGListSectionType {
+    
+    private var viewModel: MediaObjectViewModel?
+    
+    func numberOfItems() -> Int {
+        return 1
+    }
+    
+    func sizeForItem(at index: Int) -> CGSize {
+        let ccw = collectionContext!.containerSize.width
+        let width = (ccw/2.0) - 10
+        return CGSize(width: width, height: width)
+    }
+    
+    func cellForItem(at index: Int) -> UICollectionViewCell {
+        let cell = collectionContext!.dequeueReusableCell(withNibName: "ThumbMediaObjectCell", bundle: nil, for: self, at: index) as! ThumbMediaObjectCell
+        if let viewModel = viewModel {
+            cell.setCell(with: viewModel)
+        }
+        return cell
+    }
+    
+    func didUpdate(to object: Any) {
+        if let object = object as? MediaObjectViewModel {
+            self.viewModel = object
+        }
+    }
+    
+    func didSelectItem(at index: Int) {}
+    
 }
